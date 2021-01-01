db010aebabca7d8362b6c885e80bef4409587fb6
Harshit kumar <harshitrudrapur@gmail.com>
2021-01-01 19:48:41 +0530

    added card components

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,7 +1,12 @@
 <template>
- <div class="project" @click="togglecard">
+ <div class="project">
      <div class="actions">
-         <h3>{{project.title}}</h3>
+         <h3 @click="togglecard">{{project.title}}</h3>
+         <div class="icons">
+             <span class="material-icons">edit</span>
+             <span class="material-icons">delete</span>
+             <span class="material-icons">done</span>
+         </div>
      </div>
      <div class="details" v-if="showcard">
          <p>{{project.details}}</p>
@@ -40,4 +45,18 @@ export default {
  .details{
      margin: auto;
  }
+.actions{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.material-icons{
+    font-size: 24px;
+    margin: 0 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+.material-icons:hover{
+    color: #777;
+}
 </style>
